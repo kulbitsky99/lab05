41b260b1636ef757e11674b02a1707976b8319a4
kulbitsky99 <kulbitskij.na@phystech.edu>
2020-04-10 21:30:14 +0300

    removed old _build

--- _build/third-party/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mikita/2020/acronis/industrial_programming_acronis_2020/kulbitsky99/workspace/projects/lab05/third-party/gtest/googletest
-# Build directory: /home/mikita/2020/acronis/industrial_programming_acronis_2020/kulbitsky99/workspace/projects/lab05/_build/third-party/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
